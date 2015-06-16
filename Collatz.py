8027d21d2865aac21f421efa25f1a7caffed6186
Chris LeSage <crlesage@gmail.com>
2015-06-16 17:53:11 -0500

    Implemented eager cache and lazy cache.

--- Collatz.py
+++ Collatz.py
@@ -6,6 +6,35 @@
 # Glenn P. Downing
 # ---------------------------
 
+import math
+
+# ---
+# Global Cache 
+# ---
+cache = [None] * 1000000
+# Base cases
+cache[1:2] = [1, 2]
+# Eager cache, powers of 2
+cache[4] = 3
+cache[8] = 4
+cache[16] = 5
+cache[32] = 6
+cache[64] = 7
+cache[128] = 8
+cache[256] = 9
+cache[512] = 10
+cache[1024] = 11
+cache[2048] = 12
+cache[4096] = 13
+cache[8192] = 14
+cache[16384] = 15
+cache[32768] = 16
+cache[65536] = 17
+cache[131072] = 18
+cache[262144] = 19
+cache[524288] = 20
+
+
 # ------------
 # collatz_read
 # ------------
@@ -32,42 +61,61 @@ def collatz_eval (i, j) :
     # <your code>
     assert i > 0
     assert j > 0
-    assert i <= 1000000
-    assert j <= 1000000
-
-    max_cycle_length = 1
-    cycle_length = 1
+    assert i < 1000000
+    assert j < 1000000
 
     #Swap i and j if j is smaller
     if (i > j) :
         temp = i
         i = j
         j = temp
+    assert j >= i
+
+    max_cycle_length = 0;
 
     for n in range (i, j + 1) :
-        cycle_length = collatz_computeCycleLength (n)
+        if (cache[n] == None) :
+            cache[n] = collatz_helper (n)
+        if cache[n] > max_cycle_length :
+            max_cycle_length = cache[n]
 
-        if cycle_length > max_cycle_length :
-            max_cycle_length = cycle_length
-
-    assert cycle_length > 0
     assert max_cycle_length > 0
 
     return max_cycle_length
 
 # Computes and returns cycle length of n.
-def collatz_computeCycleLength (n) :
+def collatz_helper (n) :
     assert n > 0
+
+    # Cycle length less than 3 is itself
+    # if (n < 3) :
+    #     return n
+
+    # Compute cycle length otherwise
     cycle_length = 1
-    while n > 1 :
+    temp = n
+    # If not in cache already, find cycle length
+    while (cache[n] == None) :
         if (n % 2) == 0 :
-            n = n // 2
+            # Computes if power of 2, cycle length = (log base 2 of (n) + 1)
+            # Maybe just make eager cache and skip this step (not many values)
+            # if ((n & (n - 1)) == 0) and (n != 0) :
+            #     return (int)(math.log(n, 2) + 1)
+            n = (n // 2)
+            cycle_length += 1
+
         else :
-            n = (3 * n) + 1
-        cycle_length += 1
+            n += (n >> 1) + 1
+            cycle_length += 2
+
+        # Add in cache
+        cache[temp] = cycle_length + 1
 
     assert cycle_length > 0
-    return cycle_length
+
+    # Return the current cycle length plus if there is a cache value (minus one for repeat)
+    #cache_val = (int)cache[temp]
+    return cycle_length + cache[n] - 1
 
 # -------------
 # collatz_print
